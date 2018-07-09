727caafd1602152fe4a230095f50f582c2d5624d
dcarnero <40179256+dcarnero@users.noreply.github.com>
2018-07-10 00:48:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,26 @@
-var http = require('http');
+//First, we need to inject our plugin within homebridge. DCteletask is the javascript object that will contain our control logic
 var Accessory, Service, Characteristic, UUIDGen;
 //-------------------------------
 var Teletask = require('node-teletask');
-var HOST = '192.168.1.5';
-var PORT = 55957;
-var teletask = new Teletask.connect(HOST,PORT);
+
 //-------------------------------
 module.exports = function (homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   UUIDGen = homebridge.hap.uuid;
-
-homebridge.registerPlatform("DCteletask-switch-plugin", "DCteletask-Switch", DCteletask, true);
+  homebridge.registerAccessory("DCteletask-switch-plugin", "DCteletask-Switch", DCteletask);
 };
-//----------------------------------------------------
-
-
-
-//-------------------------------------
-function DCteletask-Switch(log, config, api) {
-  log("DCteletask-Switch Init");
-  var platform = this;
-  this.log = log;
-  this.config = config;
-  this.accessories = [];
-//----------------------------------------
+//---------------
 
 Switch.prototype = {
   getServices: function () {
     let informationService = new Service.AccessoryInformation();
     informationService
-      .setCharacteristic(Characteristic.Manufacturer, "My switch manufacturer")
-      .setCharacteristic(Characteristic.Model, "My switch model")
+      .setCharacteristic(Characteristic.Manufacturer, "DC")
+      .setCharacteristic(Characteristic.Model, "DCteletask")
       .setCharacteristic(Characteristic.SerialNumber, "123-456-789");
  
-    let switchService = new Service.Switch("My switch");
+    let switchService = new Service.Switch("RELE");
     switchService
       .getCharacteristic(Characteristic.On)
         .on('get', this.getSwitchOnCharacteristic.bind(this))
@@ -44,17 +30,19 @@ Switch.prototype = {
     this.switchService = switchService;
     return [informationService, switchService];
   }
-  -----------------------------------------------------
-  const request = require('request');
-const url = require('url');
- 
-function mySwitch(log, config) {
+//  -----------------------------------------------------
+var HOST = '192.168.1.5';
+var PORT = 55957;
+var teletask = new Teletask.connect(HOST,PORT);
+
+function DCteletask(log, config) {
   this.log = log;
-  this.getUrl = url.parse(config['getUrl']);
-  this.postUrl = url.parse(config['postUrl']);
+  this.get = teletask.get(Teletask.functions.relay, 1, function(report){console.log("Relay 21 is " + report.value.name);});
+  this.set = teletask.set(Teletask.functions.relay, 1, Teletask.settings.toggle);
 }
- 
-mySwitch.prototype = {
+
+
+DCteletask.prototype = {
  
   getSwitchOnCharacteristic: function (next) {
     const me = this;
@@ -62,6 +50,52 @@ mySwitch.prototype = {
         url: me.getUrl,
         method: 'GET',
     }, 
+    function (error, response, body) {
+      if (error) {
+        me.log('STATUS: ' + response.statusCode);
+        me.log(error.message);
+        return next(error);
+      }
+      return next(null, body.currentState);
+    });
+  },
+   
+  setSwitchOnCharacteristic: function (on, next) {
+    const me = this;
+    request({
+      url: me.postUrl,
+      body: {'targetState': on},
+      method: 'POST',
+      headers: {'Content-type': 'application/json'}
+    },
+    function (error, response) {
+      if (error) {
+        me.log('STATUS: ' + response.statusCode);
+        me.log(error.message);
+        return next(error);
+      }
+      return next();
+    });
+  }
+};
+// -------------  
+
+ 
+function DCteletask(log, config) {
+  this.log = log;
+  this.getUrl = url.parse(config['getUrl']);
+  this.postUrl = url.parse(config['postUrl']);
+}
+ 
+DCteletask.prototype = {
+ 
+  getSwitchOnCharacteristic: function (next) {
+    const me = this;
+    request({
+        url: me.getUrl,
+        method: 'GET',
+    }, 
+            
     function (error, response, body) {
       if (error) {
         me.log('STATUS: ' + response.statusCode);
